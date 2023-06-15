0606ace8091af07fc2006f3f989caaf0bcb3803f
kevinokello <okellokevin77@gmail.com>
2023-06-15 20:11:18 +0300

    dashboard setup

---
+++ backend/dashboard/index.php
@@ -0,0 +1,80 @@
+<?php include "../inc/app.php"; ?>
+
+<div id="wrapper">
+        <!-- Sidebar -->
+        <?php include "../inc/sidebar.php"; ?>
+
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Topbar -->
+
+
+                <?php include "../inc/header.php"; ?>
+
+                <!-- End of Topbar -->
+                <div class="container-fluid">
+        
+        <!-- Content Row -->
+        <div class="row">
+          <!-- Earnings (Monthly) Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div
+                      class="text-xs font-weight-bold text-primary text-uppercase mb-1"
+                    >
+                      Earnings (Monthly)
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      $40,000
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- Earnings (Monthly) Card Example -->
+          <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+              <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                  <div class="col mr-2">
+                    <div
+                      class="text-xs font-weight-bold text-success text-uppercase mb-1"
+                    >
+                      Earnings (Annual)
+                    </div>
+                    <div class="h5 mb-0 font-weight-bold text-gray-800">
+                      $215,000
+                    </div>
+                  </div>
+                  <div class="col-auto">
+                    <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        
+        </div>
+
+      </div>
+
+            </div>
+            <!-- End of Main Content -->
+
+
+        </div>
+        <!-- End of Content Wrapper -->
+    </div>
