a84dd34fe8dd3c7227c7514e1cc06d97610dd265
kevinokello <okellokevin77@gmail.com>
2023-06-20 19:46:41 +0300

    category display loop

--- backend/categories/index.php
+++ backend/categories/index.php
@@ -1,61 +1,70 @@
 <?php include "../inc/app.php"; ?>
-
+<?php
+include "../config.php";
+$sql = "SELECT * FROM categories";
+$result = $conn->query($sql);
+?>
 <div id="wrapper">
-        <!-- Sidebar -->
-        <?php include "../inc/sidebar.php"; ?>
+    <!-- Sidebar -->
+    <?php include "../inc/sidebar.php"; ?>
 
-        <!-- End of Sidebar -->
+    <!-- End of Sidebar -->
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-            <!-- Main Content -->
-            <div id="content">
-                <!-- Topbar -->
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Main Content -->
+        <div id="content">
+            <!-- Topbar -->
 
 
-                <?php include "../inc/header.php"; ?>
+            <?php include "../inc/header.php"; ?>
 
-                <!-- End of Topbar -->
-                <div class="container-fluid">
+            <!-- End of Topbar -->
+            <div class="container-fluid">
                 <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary"></h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
-                                        </tr>
-                                    </thead>
-                                 
-                                    <tbody>
-                                        <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                       
-                                    </tbody>
-                                </table>
-                            </div>
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary"></h6>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Name</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <?php
+                                    if ($result->num_rows > 0) {
+                                        while ($row = $result->fetch_assoc()) {
+                                    ?>
+
+                                            <tr>
+                                                <th scope="row"><?php echo $row['id']; ?></th>
+                                                <td><?php echo $row['name']; ?></td>
+                                                <td>
+                                                    <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-primary">Edit</a>
+                                                    <a href="delete.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Delete</a>
+                                                </td>
+                                            </tr>
+                                    <?php }
+                                    }
+                                    ?>
+
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-      </div>
-
+                </div>
             </div>
-            <!-- End of Main Content -->
-
 
         </div>
-        <!-- End of Content Wrapper -->
+        <!-- End of Main Content -->
+
+
     </div>
+    <!-- End of Content Wrapper -->
+</div>
