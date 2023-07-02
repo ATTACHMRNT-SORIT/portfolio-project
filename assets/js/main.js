33f324cf8e0feb8f6f8df9e9290acd6635cf35f4
kevinokello <okellokevin77@gmail.com>
2023-07-02 19:40:39 +0300

    services modal popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,26 @@ window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
 
+const modalViews = document.querySelectorAll('.services__modal')
+const modalBtns = document.querySelectorAll('.services__button')
+const modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function(modalCLick){
+    modalViews[modalCLick].classList.add('active-modal')
+
+}
+modalBtns.forEach((mb,i)=>{
+    mb.addEventListener('click',()=>{
+        modal(i)
+    })
+})
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click',()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
